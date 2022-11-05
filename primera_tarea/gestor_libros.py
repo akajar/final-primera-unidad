82a84ba565c4499cd97ee69e1f34e53a79cfc154
Bryan Ochoa <bochoatrillo@gmail.com>
2022-11-04 22:08:45 -0500

    Se definen los metodos de Gestor_Libros como metodos de clase

--- primera_tarea/gestor_libros.py
+++ primera_tarea/gestor_libros.py
@@ -2,46 +2,62 @@ from typing import List
 from libro import Libro
 
 class Gestor_libros:
-    def __init__(self) -> None:
-        self.__ruta_archivo = "libros.csv"
+    __ruta_archivo = "libros.csv"
+    __libros= []
     
-    def cargar_archivo(self) -> List[str]:
+    @classmethod
+    def cargar_archivo(cls) -> None:
         #abre el archivo y guarda los libros en memoria
-        self.libros = ["libro1","libro2","libro3"] #es un ejemplo
+        pass
     
-    def agregar_libro(self, libro: Libro) -> None:
-        #carga un libro en memoria (self.libros)
-        self.libros.append(libro)
+    @classmethod
+    def listar_libros(cls) -> None:
+        #llama al metodo listar de cada libro en la lista
+        pass
     
-    def eliminar_libro(self,id: int) -> None:
+    @classmethod
+    def agregar_libro(cls, **datos) -> None:
+        #carga un libro en la lista de libros
+        pass
+    
+    @classmethod
+    def eliminar_libro(cls,id: int) -> None:
         #elimina un libro, segun su id, de la lista
         pass
     
-    #Filtra los libros segun en tipo: 
-    #1(id), 2(titulo), 3(autor), 4(editorial), 5(genero)
-    def buscar_libro_id(self, clave: int) -> List[Libro]:
+    #Filtra los libros segun: 
+    @classmethod
+    def buscar_libro_isbn(cls, clave: str) -> List[Libro]:
         pass
     
-    def buscar_libro_titulo(self, clave: str) -> List[Libro]:
+    @classmethod
+    def buscar_libro_titulo(cls, clave: str) -> List[Libro]:
         pass
     
-    def buscar_libro_autor(self, clave: str) -> List[Libro]:
+    @classmethod
+    def buscar_libro_autor(cls, clave: str) -> List[Libro]:
         pass
     
-    def buscar_libro_editorial(self, clave: str) -> List[Libro]:
+    @classmethod
+    def buscar_libro_editorial(cls, clave: str) -> List[Libro]:
         pass
     
-    def buscar_libro_genero(self, clave: str) -> List[Libro]:
+    @classmethod
+    def buscar_libro_genero(cls, clave: str) -> List[Libro]:
         pass
     
-    def ordenar_libros(self) -> None:
+    @classmethod
+    def ordenar_libros(cls) -> None:
         #Ordena la lista segun sus titulos
         pass
     
-    def actualizar_libro(self) -> None:
+    @classmethod
+    def actualizar_libro(cls) -> None:
         #Actualiza los datos de un libro
         pass
     
-    def grabar_archivo(self) -> None:
+    @classmethod
+    def grabar_archivo(cls) -> None:
         #Graba la lista de libros en el archivo
         pass
+    
